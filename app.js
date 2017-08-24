273c0d063c60321f8706fab94c69ac4debf81782
Lim Lok Han <lim-lok.han@ascom.com>
2017-08-24 09:01:08 +0800

    First version of application moved into github

---
+++ app.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var wiki = require('./routes/wiki');
+var catalog = require('./routes/catalog');  //Import routes for "catalog" area of site
+var compression = require('compression');
+var helmet = require('helmet');
+
+var app = express();
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var mongoDB = 'mongodb://rohanlim:123456@ds159348.mlab.com:59348/local_library';
+mongoose.connect(mongoDB,  {useMongoClient: true,
+  /* other options */
+});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(helmet());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator()); // Add this after the bodyParser middlewares!
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression()); //Compress all routes
+
+// Routing start here
+app.use('/', index);
+app.use('/users', users);
+app.use('/wiki', wiki);
+app.use('/catalog', catalog);  // Add catalog routes to middleware chain.
+
+// Example of returning userID and bookID from Url
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  // Access userId via: req.params.userId
+  // Access bookId via: req.params.bookId
+  res.send(req.params)
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
